e8867f471e453a5acb3a6bb741c0742468c21ad7
666graham666 <bushmanova.ai@phystech.edu>
2020-11-12 10:25:47 +0300

    sss

---
+++ sad4.py
@@ -0,0 +1,41 @@
+import pathlib
+import shutil
+import os
+import requests
+
+path = str(pathlib.Path().absolute())
+try:
+    shutil.rmtree(path)
+except OSError:
+    print ("no")
+else:
+    print ("norm")
+
+try:
+    os.mkdir(path)
+except OSError:
+    print ("no")
+else:
+    print ("Successfully created the directiry %s , zagr foto" % path)
+km = ['4km','9km']
+amount = 6
+
+for formatt in km:
+
+
+    kmpath = path + '//' + formatt
+
+    try:
+        os.mkdir(kmpath)
+    except OSError:
+        print ("no")
+    else:
+        print (("Successfully created the directory %s " + formatt) %(path + '//'+ formatt))
+
+    for n in range(amount):
+        fotoname = str(n) + '.png'
+        name = 'https://oceancolor.gsfc.nasa.gov/showimages/MODISA/IMAGES/SST/L3/2020/09' + str(n).zfill(2)+'/AQUA_MODIS.202009'+str(n).zfill(2)+'.L3m.DAY.SST.sst.'+formatt+'.NRT.nc.png'
+        r = requests.get(name)
+        
+        open (kmpath + '//' + fotoname, 'wb').write(r.content)
+    
